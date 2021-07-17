8678fdb2955b5daa082d9e6fdf6b0b14be7523c7
Hyunjin Cho <jhj176610@gmail.com>
2021-07-18 02:02:56 +0900

    Update related to 'count' number for referencing carrot number

--- main.js
+++ main.js
@@ -1,46 +1,52 @@
 //when the start button is clicked, the images will be showed.
 const startBtn = document.querySelector(".startBtn");
+const startBtnImg = document.querySelector(".fas");
 const character = document.querySelector(".characters");
-// const bug = document.querySelector(".bug");
-// const carrot = document.querySelector(".carrot");
+const count = document.querySelector(".count");
 
-const characterWidth = character.offsetWidth;
-const characterHeight = character.offsetHeight;
-
-console.log(characterWidth, characterHeight);
+const characterWidth = character.clientWidth;
+const characterHeight = character.clientHeight;
 
 function generateBugImage() {
-  for (var i = 0; i < 5; i++) {
+  const imgNumForBug = Math.floor(Math.random() * (10 - 1) + 1);
+
+  for (var i = 0; i < imgNumForBug; i++) {
     const bugImg = document.createElement("img");
     bugImg.setAttribute("class", "bug");
     bugImg.src = `img/bug.png`;
     character.appendChild(bugImg);
 
     const randomTopForBug = getRandomNumber(0, characterHeight - 50);
-    const randomLeftForBug = getRandomNumber(0, characterWidth - 30);
+    const randomLeftForBug = getRandomNumber(0, characterWidth - 50);
     bugImg.style.transform = `translate(${randomLeftForBug}px, ${randomTopForBug}px)`;
   }
 }
 
 function generateCarrotImage() {
-  for (var i = 0; i < 5; i++) {
+  const imgNumForCarrot = Math.floor(Math.random() * (10 - 1) + 1);
+  for (var i = 0; i < imgNumForCarrot; i++) {
     const carrotImg = document.createElement("img");
     carrotImg.setAttribute("class", "carrot");
     carrotImg.src = `img/carrot.png`;
     character.appendChild(carrotImg);
 
     const randomTopForCarrot = getRandomNumber(0, characterHeight - 50);
-    const randomLeftForCarrot = getRandomNumber(0, characterWidth - 30);
+    const randomLeftForCarrot = getRandomNumber(0, characterWidth - 50);
     carrotImg.style.transform = `translate(${randomLeftForCarrot}px, ${randomTopForCarrot}px)`;
   }
+  count.innerText = `${imgNumForCarrot}`;
 }
-
-startBtn.addEventListener("click", (event) => {
-  generateBugImage();
-  generateCarrotImage();
-});
 
 function getRandomNumber(min, max) {
-  const randomNum = Math.random() * (max - min) + min;
-  return Math.floor(randomNum);
+  return Math.random() * (max - min) + min;
 }
+
+function init() {
+  startBtn.addEventListener("click", (event) => {
+    generateBugImage();
+    generateCarrotImage();
+    startBtnImg.className = "fas fa-square";
+  });
+}
+
+init();
