e0be8324fc3bc6881b26fa0a328113b99baed1fe
Hyunjin Cho <jhj176610@gmail.com>
2021-07-20 16:37:26 +0900

    Update javascript - By clicking replay button, the game start again

--- main.js
+++ main.js
@@ -5,8 +5,11 @@ const startBtnImg = document.querySelector(".fas");
 const character = document.querySelector(".characters");
 const count = document.querySelector(".count");
 const timer = document.querySelector(".timer");
+
 const popUp = document.querySelector(".loseAndWin");
 const popUpMsg = document.querySelector(".popUpMsg");
+
+const redoBtn = document.querySelector(".redoBtn");
 
 let timer_start;
 let started = false; //default
@@ -27,6 +30,11 @@ startBtn.addEventListener("click", (event) => {
   // 게임이 진행 중인지 아닌지 알려줌.
   // 동일한 버튼을 눌렀을 때, 게임의 상태를 알지 못하면 작동이 안됨.
   // started가 true 즉 게임이 실행되면 중지 함수, false라면 시작 함수
+});
+
+redoBtn.addEventListener("click", (event) => {
+  hidePopUpMsg();
+  gameStart();
 });
 
 function gameStart() {
@@ -59,6 +67,10 @@ function showPopUpMessage(txt) {
   popUpMsg.innerText = txt;
 }
 
+function hidePopUpMsg() {
+  popUp.classList.add("loseAndWin_hide");
+}
+
 function hideStartBtn() {
   startBtn.style.visibility = "hidden";
 }
@@ -88,16 +100,17 @@ function getRandomNumber(min, max) {
 }
 
 function startCountdown() {
-  timer.innerHTML = `00:${seconds < 10 ? `0${seconds}` : seconds}`;
+  let leftTime = seconds;
+  timer.innerHTML = `00:${leftTime < 10 ? `0${leftTime}` : leftTime}`;
   timer_start = setInterval(second_start, 1000);
 
   function second_start() {
-    if (seconds <= 0) {
+    if (leftTime <= 0) {
       clearInterval(timer_start);
       return;
     }
-    --seconds;
-    timer.innerHTML = `00:${seconds < 10 ? `0${seconds}` : seconds}`;
+    --leftTime;
+    timer.innerHTML = `00:${leftTime < 10 ? `0${leftTime}` : leftTime}`;
   }
 }
 
