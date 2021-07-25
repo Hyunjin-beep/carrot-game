547b642fa70633fe9b70660eb56143eef588ab41
Hyunjin Cho <jhj176610@gmail.com>
2021-07-25 15:55:10 +0900

    Refactoring - Create game.js for all functions except for popup and character

---
+++ src/game.js
@@ -0,0 +1,129 @@
+"use strict";
+
+import Character from "./character.js";
+import * as sound from "./sound.js";
+
+export default class Game {
+  constructor() {
+    this.startBtn = document.querySelector(".startBtn");
+    this.startBtnImg = document.querySelector(".fas");
+
+    this.startBtn.addEventListener("click", () => {
+      if (this.started) {
+        // true
+        this.gameStop(this.replay);
+      } else {
+        // false
+        this.gameStart();
+      }
+      // 게임이 진행 중인지 아닌지 알려줌.
+      // 동일한 버튼을 눌렀을 때, 게임의 상태를 알지 못하면 작동이 안됨.
+      // started가 true 즉 게임이 실행되면 중지 함수, false라면 시작 함수
+    });
+
+    this.gameCharacter = new Character();
+    this.gameCharacter.setClickListener((event) => {
+      this.onCharacterClick(event);
+    });
+
+    this.count = document.querySelector(".count");
+    this.timer = document.querySelector(".timer");
+
+    this.replay = "cancel";
+    this.lose = "lose";
+    this.win = "win";
+
+    this.timer_start;
+    this.started = false; //default
+    this.seconds = 10;
+  }
+
+  setGameReasonListener(onGameStop) {
+    this.onGameStop = onGameStop;
+  }
+
+  onCharacterClick(item) {
+    if (!this.started) {
+      return;
+    }
+    if (item === "bug") {
+      this.gameStop(this.lose);
+    } else if (item === "carrot") {
+      const carrot = document.querySelectorAll(".carrot");
+      const initialCarrot = carrot.length;
+      this.updateScore(initialCarrot);
+    }
+  }
+
+  gameStart() {
+    this.started = true;
+    this.initGame();
+    this.showStartBtn();
+    this.startBtnImg.className = "fas fa-square";
+    this.startCountdown();
+    sound.playBg();
+  }
+
+  gameStop(txt) {
+    this.started = false;
+    this.stopCountdown();
+    this.hideStartBtn();
+    this.onGameStop && this.onGameStop(`${txt}`);
+    sound.playAlert();
+    sound.stopBg();
+  }
+
+  initGame() {
+    //게임을 새로 시작할 때마다, 필드는 비게 되어서 캐릭터들이 재배치
+    this.showTimerAndCount();
+    this.gameCharacter.init();
+  }
+
+  showTimerAndCount() {
+    this.timer.style.visibility = "visible";
+    this.count.style.visibility = "visible";
+  }
+
+  showStartBtn() {
+    this.startBtn.style.visibility = "visible";
+  }
+
+  hideStartBtn() {
+    this.startBtn.style.visibility = "hidden";
+  }
+
+  startCountdown() {
+    let leftTime = this.seconds;
+    this.timer.innerHTML = `00:${leftTime < 10 ? `0${leftTime}` : leftTime}`;
+    this.timer_start = setInterval(this.second_start(leftTime), 1000);
+  }
+
+  second_start(leftTime) {
+    if (leftTime == 0) {
+      this.gameStop(lose);
+      sound.playBug();
+      clearInterval(this.timer_start);
+
+      return;
+    }
+    --leftTime;
+    this.timer.innerHTML = `00:${leftTime < 10 ? `0${leftTime}` : leftTime}`;
+  }
+
+  stopCountdown() {
+    clearInterval(this.timer_start);
+  }
+
+  updateScore(initialCarrot) {
+    let leftCarrot = initialCarrot;
+    --leftCarrot;
+
+    this.count.innerText = leftCarrot + 1;
+
+    if (leftCarrot === -1) {
+      sound.playWin();
+      this.gameStop(this.win);
+      return;
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,123 +1,31 @@
 "use strict";
 
 import PopUp from "./popUp.js";
-import Character from "./character.js";
-import * as sound from "./sound.js";
+import Game from "./game.js";
 
 //when the start button is clicked, the images will be showed.
-const startBtn = document.querySelector(".startBtn");
-const startBtnImg = document.querySelector(".fas");
-
-const count = document.querySelector(".count");
-const timer = document.querySelector(".timer");
-
-const replay = "REPLAY ❓";
-const lose = "YOU LOST ☢";
-const win = "YOU WON ❤";
-
-let timer_start;
-let started = false; //default
-let seconds = 10;
 
 const gameFinishPopUp = new PopUp();
-gameFinishPopUp.setClickListener(gameStart);
-
-const gameCharacter = new Character();
-gameCharacter.setClickListener((event) => {
-  onCharacterClick(event);
+gameFinishPopUp.setClickListener(() => {
+  game.gameStart();
 });
 
-startBtn.addEventListener("click", (event) => {
-  if (started) {
-    // true
-    gameStop(replay);
-  } else {
-    // false
-    gameStart();
+const game = new Game();
+game.setGameReasonListener((reason) => {
+  switch (reason) {
+    case "cancel":
+      gameFinishPopUp.showMsg("REPLAY ❓");
+      break;
+
+    case "lose":
+      gameFinishPopUp.showMsg("YOU LOST ☢");
+      break;
+
+    case "win":
+      gameFinishPopUp.showMsg("YOU WON ❤");
+      break;
+
+    default:
+      throw new Error("not valid reason");
   }
-  // 게임이 진행 중인지 아닌지 알려줌.
-  // 동일한 버튼을 눌렀을 때, 게임의 상태를 알지 못하면 작동이 안됨.
-  // started가 true 즉 게임이 실행되면 중지 함수, false라면 시작 함수
 });
-
-function onCharacterClick(item) {
-  if (!started) {
-    return;
-  }
-  if (item === "bug") {
-    gameStop(lose);
-  } else if (item === "carrot") {
-    updateScore(initialCarrot);
-  }
-}
-
-function updateScore(initialCarrot) {
-  let leftCarrot = initialCarrot;
-  --leftCarrot;
-
-  count.innerText = leftCarrot + 1;
-
-  if (leftCarrot === -1) {
-    sound.playWin();
-    gameStop(win);
-    return;
-  }
-}
-
-function gameStart() {
-  started = true;
-  initGame();
-  showStartBtn();
-  startBtnImg.className = "fas fa-square";
-  startCountdown();
-  sound.playBg();
-}
-
-function gameStop(txt) {
-  started = false;
-  stopCountdown();
-  hideStartBtn();
-  gameFinishPopUp.showMsg(txt);
-  sound.playAlert();
-  sound.stopBg();
-}
-
-function initGame() {
-  //게임을 새로 시작할 때마다, 필드는 비게 되어서 캐릭터들이 재배치
-  showTimerAndCount();
-  gameCharacter.init();
-}
-
-function showTimerAndCount() {
-  timer.style.visibility = "visible";
-  count.style.visibility = "visible";
-}
-
-function showStartBtn() {
-  startBtn.style.visibility = "visible";
-}
-
-function hideStartBtn() {
-  startBtn.style.visibility = "hidden";
-}
-
-function startCountdown() {
-  let leftTime = seconds;
-  timer.innerHTML = `00:${leftTime < 10 ? `0${leftTime}` : leftTime}`;
-  timer_start = setInterval(second_start, 1000);
-
-  function second_start() {
-    if (leftTime == 0) {
-      gameStop(lose);
-      sound.playBug();
-      clearInterval(timer_start);
-      return;
-    }
-    --leftTime;
-    timer.innerHTML = `00:${leftTime < 10 ? `0${leftTime}` : leftTime}`;
-  }
-}
-
-function stopCountdown() {
-  clearInterval(timer_start);
-}
